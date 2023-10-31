9482344e69cb1e4915e63e5662723c1b6f4d23d4
Tyler Huyser <tyler.huyser@gmail.com>
2023-10-30 22:24:16 -0400

    renamed css sheets.

--- src/components/SelectInfluencerContent.js
+++ src/components/SelectInfluencerContent.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useStaticQuery, graphql, Link } from 'gatsby';
-
+ 
 import "../styles/SelectInfluencerContent.css"
 
 export default function SelectInfluencerContent() {
