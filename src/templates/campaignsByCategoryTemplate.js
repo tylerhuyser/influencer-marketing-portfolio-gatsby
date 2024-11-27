a7a8fd95fa3beb708632b298161281ee9d83e1fd
Tyler Huyser <tyler.huyser@gmail.com>
2024-11-27 14:09:33 -0500

    added styling to CampaignsByCategory title. Had to place that page inside of a container as well.

--- src/templates/campaignsByCategoryTemplate.js
+++ src/templates/campaignsByCategoryTemplate.js
@@ -3,15 +3,21 @@ import React from "react"
 import Layout from '../components/shared/Layout'
 import CampaignCards from '../components/CampaignCards'
 
+import "../styles/CampaignsByCategory.css"
+
 export default function CampaignByCategoryTemplate({ pageContext }) { 
 
   return (  
 
     <Layout loaded={true} isMounted={true} title={`${pageContext.campaignCategory.charAt(0).toUpperCase()}${pageContext.campaignCategory.slice(1)} | Influencer Campaigns`} >
 
-      <h1>{`${pageContext.campaignCategory.charAt(0).toUpperCase()}${pageContext.campaignCategory.slice(1)} Campaigns`}</h1>
-      
-      <CampaignCards category={`${pageContext.campaignCategory}`} />
+      <div className="campaigns-by-category-container" id={`${pageContext.campaignCategory}-campaigns-container`}>
+
+        <h1 className="campaigns-by-category-title">{`${pageContext.campaignCategory.charAt(0).toUpperCase()}${pageContext.campaignCategory.slice(1)} Campaigns`}</h1>
+        
+        <CampaignCards category={`${pageContext.campaignCategory}`} />
+
+      </div>
           
     </Layout>
 
